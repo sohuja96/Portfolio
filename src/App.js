392d2df17936cb8345e5ff0d23d460cd1d9a463e
sohuja96 <jsantillo3@gatech.edu>
2019-01-10 21:31:20 -0500

    Sepia filter over images

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ render() {
             <div className="textbox">
             <h3>Eli Rice Seeder</h3>
             <h4>Fall 2018</h4>
-            <p>For the second semester of Engineering Social Innovation, I worked on improving a crop planter designed to work in complex environmental conditions. We were able model a new steering and planting system that reduced the amount of manpower required to maintain a consistent agricultural output.</p>
+            <p>Modeled a new steering and planting system compatable with wet and dry seasons.</p>
             <h6><a href="https://github.com/sohuja96/gtvip/tree/fall18">View it on Github</a></h6>
             </div>
           </div>
@@ -72,7 +72,7 @@ render() {
             <div className="textbox">
             <h3>Community Safety Lighting</h3>
             <h4>Spring 2018</h4>
-            <p>For my Engineering Social Innovation project, my team collaboratively designed a modular lighting system for the sake of neighborhood visibility and safety at night. Proof-of-concept experiments were conducted to show the block-components of how the product would work on a circuit level.</p>
+            <p>Designed a modular lighting system for community safety.</p>
             <h6><a href="https://github.com/sohuja96/gtvip/tree/spring18">See it on Github</a></h6>
             </div>
           </div>
@@ -80,7 +80,7 @@ render() {
             <div className="textbox">
             <h3>Data Visualizations</h3>
             <h4>Spring 2018</h4>
-            <p>Used D3.js to create interactive and informative visualizations of different datasets.</p>
+            <p>Used D3.js to create interactive and informative visualizations of high-dimensional datasets.</p>
             <h6><a href="https://github.com/sohuja96/Data-Visualizations">Inspect it on Github</a></h6>
             </div>
           </div>
@@ -88,7 +88,7 @@ render() {
             <div className="textbox">
             <h3>Order-of-Magnitude Classifier</h3>
             <h4>Spring 2018</h4>
-            <p>Developed a configurable CLI for learning the order of magnitude of a dataset using the Python machine learning library, Scikit Learn.</p>
+            <p>Developed a CLI for learning the order of magnitude of a numerical dataset using Scikit Learn.</p>
             <h6><a href="https://github.com/sohuja96/OoM-Classifier">Glimpse it on Github</a></h6>
             </div>
           </div>
